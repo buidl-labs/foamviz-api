52e5b57378d1c795085d85414e831ba2fec86b1c
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-01-30 01:02:43 +0530

    Added top five cartographer API

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ const app = express();
 app.use(cors());
 
 let vizDataGlobeData;
+let topFiveCartographerDetails;
 const mainURL = 'https://map-api-direct.foam.space/poi/filtered?swLng=-180&swLat=-90&neLng=180&neLat=90&limit=100000&offset=0&sort=oldest';
 // const mainURL = 'https://jsonplaceholder.typicode.com/todos/1';
+const allCartographerDetailAPI = 'https://api.blocklytics.org/foam/v0/cartographers?sort=points_on_map&key=AIzaSyAz1sT-EtRPbRlTpNAw3OHNYz463vyA-I0';
 
 app.listen(port, function () {
   console.log("Server is running on " + port + " port");
 });
 
 async function fetchViz3Data() {
-  // const res = await fetch(mainURL).then((res) => res.json().catch((err) => console.log('error', err)));
   try {
     const response = await fetch(mainURL, {
       timeout: 100000
@@ -28,18 +29,39 @@ async function fetchViz3Data() {
   }
 }
 
+async function fetchCartographerDetails() {
+  try {
+    const response = await fetch(allCartographerDetailAPI, {
+      timeout: 100000
+    });
+    const json = await response.json();
+    return json.slice(0, 5);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 setInterval(() => {
-  console.log('Fetch data for Viz 3')
+  // console.log('Fetched new data')
   try {
     fetchViz3Data().then((allData) => {
       vizDataGlobeData = allData;
     });
+
+    fetchCartographerDetails().then((data) => {
+      topFiveCartographerDetails = data;
+    });
   } catch (err) {
     reject(err)
   }
-}, 1000 * 60)
+}, 1000 * 60 * 15)
 
 app.get('/', function (req, res) {
-  console.log('data updated on server')
+  // console.log('data updated on server')
   res.json(vizDataGlobeData)
+})
+
+app.get('/top-cartographer-details', function (req, res) {
+  // console.log('top-five-cartographer-details')
+  res.json(topFiveCartographerDetails)
 })
